6303eb897cd085a9bcb1179a0cc9b7c600b7f89f
Dreary Lisper <drearylisper@gmail.com>
2025-10-16 20:37:45 +0100

    Day 20, Part 1.

--- src/20/main.rs
+++ src/20/main.rs
@@ -21,6 +21,9 @@ fn parse(content: &String) -> State {
     let mut map: Vec<Vec<char>> = vec![];
 
     for (row, line) in lines.enumerate() {
+        if line.is_empty() {
+            break;
+        }
         if let Some(column) = line.find("S") {
             start = (row as i32, column as i32);
         }
@@ -140,7 +143,16 @@ fn search2(state: &State) -> Vec<(i32, Vec<(i32, i32)>)> {
         //println!("{}", length);
 
         if state.end == (x, y) {
-            result.push((length, cheat.clone()));
+            let mut s = false;
+            for (_, c) in result.clone() {
+                if c == *cheat {
+                    s = true;
+                }
+            }
+
+            if !s {
+                result.push((length, cheat.clone()));
+            }
 
             if cheat.len() == 0 {
                 break;
@@ -204,9 +216,113 @@ fn search2(state: &State) -> Vec<(i32, Vec<(i32, i32)>)> {
     result
 }
 
+fn search3(state: &State) -> Vec<(i32, Vec<(i32, i32)>)> {
+    let mut queue = BinaryHeap::new();
+
+    let search_state = SearchState {
+        point: state.start,
+        cheat: vec![],
+        seen: Rc::new(RefCell::new(HashSet::new())),
+        length: 0,
+    };
+
+    queue.push(search_state);
+
+    let mut result = vec![];
+
+    while queue.len() > 0 {
+        let mut search_state = queue.pop().unwrap();
+
+        let (x, y) = search_state.point;
+        let length = search_state.length;
+        let cheat = &search_state.cheat;
+
+        let mut seen = search_state.seen.borrow_mut();
+
+        //println!("{}", length);
+
+        if state.end == (x, y) {
+            let mut s = false;
+            for (_, c) in result.clone() {
+                if c == *cheat {
+                    s = true;
+                }
+            }
+
+            if !s {
+                result.push((length, cheat.clone()));
+            }
+
+            if cheat.len() == 0 {
+                break;
+            }
+        }
+
+        for (dx, dy) in vec![(-1, 0), (1, 0), (0, -1), (0, 1)] {
+            let (nx, ny) = (x + dx, y + dy);
+
+            if nx < 0 || nx >= state.map.len() as i32 {
+                continue;
+            }
+
+            if ny < 0 || ny >= state.map[0].len() as i32 {
+                continue;
+            }
+
+            if seen.contains(&(nx, ny)) {
+                continue;
+            }
+
+            if state.map[nx as usize][ny as usize] == '#' {
+                if cheat.len() < 20 {
+                    if cheat.len() == 0 {
+                        let mut nseen = (*seen).clone();
+                        nseen.insert((nx, ny));
+
+                        let mut ncheat = cheat.clone();
+                        ncheat.push((nx, ny));
+
+                        let search_state = SearchState {
+                            point: (nx, ny),
+                            cheat: ncheat,
+                            seen: Rc::new(RefCell::new(nseen)),
+                            length: length - 1,
+                        };
+
+                        queue.push(search_state);
+                    }
+                } else {
+                    continue;
+                }
+            } else {
+                seen.insert((nx, ny));
+
+                let mut ncheat = cheat.clone();
+                if cheat.len() > 0 {
+                    ncheat.push((nx, ny));
+                }
+
+                let search_state = SearchState {
+                    point: (nx, ny),
+                    cheat: ncheat,
+                    seen: search_state.seen.clone(),
+                    length: length - 1,
+                };
+
+                queue.push(search_state);
+            }
+        }
+    }
+
+    result
+}
+
 fn part01(content: &String) -> i32 {
     let state = parse(content);
     let mut paths = search2(&state);
+
+    paths.sort();
+    paths.dedup();
 
     println!("{:?}", paths);
     //println!("{:?}", paths.iter().map(|i| i.0).collect::<Vec<_>>());
@@ -215,19 +331,34 @@ fn part01(content: &String) -> i32 {
 
     paths
         .iter()
-        .filter(|(i, _)| i - longest >= 100)
+        .filter(|(i, _)| (-longest) + i >= 100)
         .collect::<Vec<_>>()
         .len() as i32
 }
 
 fn part02(content: &String) -> i32 {
-    0
+    let state = parse(content);
+    let mut paths = search3(&state);
+
+    paths.sort();
+    paths.dedup();
+
+    println!("{:?}", paths);
+    //println!("{:?}", paths.iter().map(|i| i.0).collect::<Vec<_>>());
+
+    let longest = paths.iter().filter(|i| i.1.len() == 0).last().unwrap().0;
+
+    paths
+        .iter()
+        .filter(|(i, _)| (-longest) + i >= 100)
+        .collect::<Vec<_>>()
+        .len() as i32
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let content = aoc_2024::io::read_input("20")?;
+    let content = aoc_2024::io::read_test("20")?;
 
-    println!("{}", part01(&content));
+    //println!("{}", part01(&content));
     println!("{}", part02(&content));
 
     Ok(())
